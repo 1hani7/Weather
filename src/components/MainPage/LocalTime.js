4c2c3658025a56899cd8f39728a51b7d4fedd4df
1hani7 <1hani7@naver.com>
2023-11-03 15:04:07 +0900

    dsa

---
+++ src/components/MainPage/LocalTime.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/LocalTime.css';
+
+export default function LocalTime(){
+    return(
+        <div className="Container">
+
+        </div>
+    )
+};
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './css/MainPage.css';
 
+import LocalTime from '../components/MainPage/LocalTime';
+
 export default function MainPage(){
-    
+    return(
+        <div className="MainPage">
+            <LocalTime></LocalTime>
+        </div>
+    )
 }
